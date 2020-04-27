56ca72c4c85cc33b798e6c1c203b68e29a3c2743
Eliezer <beczi_ezi@yahoo.com>
2020-04-28 00:06:54 +0300

    started using pseudocode

--- progfiles/single-objective-cndp/greedy/greedy_algorithm.py
+++
@@ -1,9 +0,0 @@
-def greedy_cnp(G, k):
-    S = networkx.algorithms.approximation.min_weighted_vertex_cover(G)
-
-    while len(S) > k:
-        B = objective_function.minimize_pairwise_connectivity(G, S)
-        i = random.choice(B)
-        S.discard(i)
-
-    return S
